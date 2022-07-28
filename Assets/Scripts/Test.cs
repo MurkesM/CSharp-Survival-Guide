7db773156a981b5e6f9f33c4ad20f24244655311
unknown <ethanmartin377@yahoo.com>
2022-07-28 14:36:36 -0500

    testing structs vs classes along with value vs reference types

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    ExampleClass exampleClass = new ExampleClass(5, "default");
+
+    ExampleStruct exampleStruct;
+
+    void Start()
+    {
+        exampleStruct.testInt = 5;
+        exampleStruct.testString = "default";
+
+        Debug.Log($"Struct testString before change = {exampleStruct.testString}");
+
+        ChangeValue(exampleStruct);
+
+        Debug.Log($"Struct testString after change = {exampleStruct.testString}");
+    }
+    void ChangeValue(ExampleStruct exampleStruct) //value type
+    {
+        exampleStruct.testString = "updated";
+        Debug.Log(exampleStruct.testString);
+    }
+
+    void ChangeValue(ExampleClass exampleClass) //reference type
+    {
+        exampleClass.testString = "updated";
+    }
+}
